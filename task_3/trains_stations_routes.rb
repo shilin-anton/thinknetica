a6e93b909350a4fe96f7fffc5b4b10cfd2dda56d
Anton <anton.d.shilin@gmail.com>
2022-05-21 15:31:19 +0300

    fixes

--- task_3/trains_stations_routes.rb
+++ task_3/trains_stations_routes.rb
@@ -31,16 +31,16 @@
 =end
 
 class Station
-  attr_reader :trains_list, :title
+  attr_reader :trains, :name
 
-  def initialize(title)
-    @title = title
-    @trains_list = []
+  def initialize(name)
+    @name = name
+    @trains = []
   end
 
   def add_train(train)
-    puts "На станции #{@title} появился поезд #{train.number} типа #{train.type} с кол-вом вагонов == #{train.cars_amount}"
-    @trains_list << train
+    puts "На станции #{@name} появился поезд #{train.number} типа #{train.type} с кол-вом вагонов == #{train.cars_amount}"
+    @trains << train
   end
 
 
@@ -48,7 +48,7 @@ class Station
     trains_amount = 0
     trains_by_type_list = []
 
-    @trains_list .each do |train|
+    @trains.each do |train|
       if train.type == type
         trains_amount+= 1
         trains_by_type_list << train
@@ -60,50 +60,44 @@ class Station
   end
 
   def train_departure(train)
-    if @trains_list.include?(train)
-      puts "Поезд №#{train.number} отправился со станции #{@title}"
-      @trains_list.delete(train)
+    if @trains.include?(train)
+      puts "Поезд №#{train.number} отправился со станции #{@name}"
+      @trains.delete(train)
     else
-      puts "Нет поезда №#{train.number} на станции #{@title}"
+      puts "Нет поезда №#{train.number} на станции #{@name}"
     end
   end
 
 end
 
 class Route
-  attr_reader :start_station, :end_station
-  @@intermediate_stations = []
+  attr_accessor :stations
 
   def initialize(start_station, end_station)
-    @start_station = start_station
-    @end_station = end_station
+    @stations = [start_station, end_station]
   end
 
   def add_station(station)
-    @@intermediate_stations.push(station)
+    @stations.insert(-2, station)
   end
 
   def delete_station(station)
-    if @@intermediate_stations.include?(station)
-      puts "Станция #{station.title} удалена из маршрута!"
-      @@intermediate_stations.delete(station)
+    if @stations.include?(station)
+      puts "Станция #{station.name} удалена из маршрута!"
+      @stations.delete(station)
     else
-      puts "Станции #{station.title} нет в промежуточном маршруте!"
+      puts "Станции #{station.name} нет в промежуточном маршруте!"
     end
   end
 
   def get_stations
-    stations = []
-    stations.push(@start_station).push(@@intermediate_stations).push(@end_station)
-    stations = stations.flatten
-    stations
+    @stations.each { |s| puts s.name } 
   end
 end
 
 class Train
   attr_reader :number, :type, :cars_amount, :route
   attr_accessor :speed
-  @@current_station = nil
 
   def initialize(number, type, cars_amount)
     @speed = 0
@@ -127,22 +121,23 @@ class Train
   end
 
   def remove_car
-    @cars_amount-= 1 if (speed == 0 && @cars_amount > 0)
+    @cars_amount-= 1 if (speed == 0 && @cars_amount > 1)
   end
 
   def set_route(route)
     @route = route
-    puts "Поезд №#{@number} отправляется по маршруту #{@route}. Начальная станция: #{@route.start_station.title}"
-    @@current_station = @route.start_station
-    @route.start_station.add_train(self)
+    stations = @route.get_stations
+    puts "Поезд №#{@number} отправляется по маршруту #{@route}. Начальная станция: #{stations.first.name}"
+    @current_station = stations.first
+    stations.first.add_train(self)
   end
 
   def get_current_station
     if @route.nil?
       puts "У поезда №#{@number} не задан маршрут!"
     else
-      puts "Поезд №#{@number} сейчас находится на станции #{@@current_station.title}"
-      @@current_station
+      puts "Поезд №#{@number} сейчас находится на станции #{@current_station.name}"
+      @current_station
     end
   end
 
@@ -152,13 +147,13 @@ class Train
     if @route.nil?
       puts "У поезда №#{@number} не задан маршрут!"
     else
-      if @@current_station == @route.end_station
+      stations = @route.get_stations
+      if @current_station == stations.last
         puts "У поезда №#{@number} нет следующей станции! Он находится в конце маршрута!"
       else
-        stations = @route.get_stations
-        index = stations.index(@@current_station)
+        index = stations.index(@current_station)
         next_station = stations[index+1]
-        puts "Следующая станция для поезда №#{number}: #{next_station.title}"
+        puts "Следующая станция для поезда №#{number}: #{next_station.name}"
       end
     end
     next_station
@@ -170,13 +165,13 @@ class Train
     if @route.nil?
       puts "У поезда №#{@number} не задан маршрут!"
     else
-      if @@current_station == @route.start_station
+      stations = @route.get_stations
+      if @current_station == stations.first
         puts "У поезда №#{@number} нет предыдущей станции! Он находится в начале маршрута!"
       else
-        stations = @route.get_stations
-        index = stations.index(@@current_station)
+        index = stations.index(@current_station)
         prev_station = stations[index-1]
-        puts "Предыдущая станция для поезда №#{number}: #{prev_station.title}"
+        puts "Предыдущая станция для поезда №#{number}: #{prev_station.name}"
       end
     end
     prev_station
@@ -188,8 +183,8 @@ class Train
     else
       next_station = get_next_station
       if !next_station.nil?
-        @@current_station.train_departure(self)
-        @@current_station = next_station
+        @current_station.train_departure(self)
+        @current_station = next_station
         next_station.add_train(self)
       end
     end
@@ -201,9 +196,9 @@ class Train
     else
       prev_station = get_previous_station
       if !prev_station.nil?
-        @@current_station.train_departure(self)
-        puts "Поезд №#{@number} отправляется назад, на станцию #{prev_station.title}"
-        @@current_station = prev_station
+        @current_station.train_departure(self)
+        puts "Поезд №#{@number} отправляется назад, на станцию #{prev_station.name}"
+        @current_station = prev_station
         prev_station.add_train(self)
       end
     end
